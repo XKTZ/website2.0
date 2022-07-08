e145fa8647715881acb07b49507c5205637fcaaf
XKTZ <ericchenxktz@gmail.com>
2022-07-08 14:32:51 -0400

    Edited the plugin of webpack, add mini css extract plugin into

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,13 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
     chainWebpack: config => {
         config.module.rule('pdf')
             .test(/\.pdf$/)
             .use('file-loader').loader('file-loader')
+    },
+    configureWebpack: {
+        plugins: [
+            new MiniCssExtractPlugin()
+        ]
     }
 }
